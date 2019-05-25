790ccc2e837ae93fc1d9ab0bfccc52d3444b4796
Sydney <sjbarney99@gmail.com>
2019-05-25 10:39:42 -0600

    first commit

--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { UPDATE_NAME, UPDATE_CATEGORY, UPDATE_CATAGORY } from "./../../store.js";
 import "./Name.css";
 
 class Name extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      name: '',
-      category: ''
+      name: reduxState.name,
+      category: reduxState.catagory
     };
   }
   handleNameChange(nameVal) {
@@ -22,7 +24,14 @@ class Name extends Component {
     });
   }
   saveChanges() {
-    // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_NAME,
+      payload: this.state.name
+    });
+    store.dispatch({
+      type: UPDATE_CATAGORY,
+      payload: this.state.category
+    });
   }
   render() {
     return (
--- src/store.js
+++ src/store.js
@@ -1,1 +1,62 @@
+import {createStore} from "redux";
 
+const intialState = {
+    name: "",
+    catagory: "",
+    authorFirst: "",
+    authorLast: "",
+    ingredients: [],
+    instructions: [],
+    recipes: []
+};
+
+export const UPDATE_NAME = "UPDATE_NAME";
+export const UPDATE_CATAGORY = "UPDATE_CATAGORY";
+export const UPDATE_AUTHOR_FIRST = "UPDATE_AUTHOR_FIRST";
+export const UPDATE_AUTHOR_LAST = "UPDATE_AUTHOR_LAST";
+export const ADD_INGREDIENT = "ADD_INGREDIENT";
+export const ADD_INSTRUCTION = "ADD_INSTRUCTION";   
+export const ADD_RECIPE = "ADD_RECIPE";
+
+const reducer = (state = intialState, action) => {
+    const {type,payload} = action;
+    switch(type){
+        case UPDATE_NAME:
+            return { ...state, name: payload };
+        case UPDATE_CATAGORY:
+            return { ...state, category: payload };
+        case UPDATE_AUTHOR_FIRST:
+            return { ...state, authorFirst: payload };
+        case UPDATE_AUTHOR_LAST:
+            return { ...state, authorLast: payload };
+        case ADD_INGREDIENT:
+            const newIngredients = [...state.ingredients, payload];
+            return { ...state, ingredients: newIngredients };
+        case ADD_INSTRUCTION:
+            const newInstructions = [...state.instructions, payload];
+            return { ...state, instructions: newInstructions };
+            case ADD_RECIPE:
+                    const {
+                      name,
+                      category,
+                      authorFirst,
+                      authorLast,
+                      ingredients,
+                      instructions
+                    } = state;
+                    const recipe = {
+                      name,
+                      category,
+                      authorFirst,
+                      authorLast,
+                      ingredients,
+                      instructions
+                    };
+                    const newRecipes = [...state.recipes, recipe];
+                    return { ...state, recipes: newRecipes };
+        default:
+            return state;
+    }
+}
+
+export default createStore(reducer);
